db64a3d48b1d7726580d26849892a1859ebc580b
munsangu <ds-3632@hanmail.net>
2021-07-18 23:56:45 +0900

    20210718

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-        //화면구성
-        let body = document.createElement('h1');
-        document.body.append(body);
-        body.textContent = '끝말잇기';
-        
-        //제시단어
-        let firstWord = document.createElement('div');
-        firstWord.textContent = '컴퓨터';
-        document.body.append(firstWord);
-        let form = document.createElement('form');
-        document.body.append(form);
-        let input = document.createElement('input');
-        form.append(input);
-        let btn = document.createElement('button');
-        btn.textContent = '클릭'
-        form.append(btn);
-        let result = document.createElement('div');
-        document.body.append(result);
+ let firstWord = document.querySelector(".firstword")
+//  console.log(firstWord.innerText)
+document.body.append(firstWord)
+ let input = document.querySelector("input")
+//  console.log(input.innerText)
+ const form = document.querySelector("form")
+//  console.log(form)
+ 
+ let result = document.createElement("span");
+ document.body.append(result);
 
-        //버튼 클릭이벤트
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-            //제시단어.textContent의 마지막 글자 = input.value의 첫글자
-            if (firstWord.textContent[firstWord.textContent.length - 1] == input.value[0]) {
-                result.textContent = '딩동댕'
-                firstWord.textContent = input.value;
-                input.value = '';
-                input.focus();
-            } else {
-                result.textContent = '땡'
-                input.value = "";
-                input.focus();
-            }
-        })
+ form.addEventListener("submit", (e) => {
+     e.preventDefault();
+     if (firstWord.innerText[firstWord.innerText.length - 1] === input.value[0]) {
+         result.innerText = "딩동댕"
+         firstWord.innerText = input.value;
+         input.value = "";
+         input.focus();
+     } else {
+         result.innerText = "땡"
+         input.value = "";
+         input.focus();
+     }
+ })
